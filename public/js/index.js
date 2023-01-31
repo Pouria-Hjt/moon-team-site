a2a68e36fe17b4b3bb1005c1cb07fe6ad3c7b42f
pouria <poriw.hjt@gmail.com>
2023-01-31 17:10:30 +0330

    add parallax effect

---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+window.onload
+    = function() {
+    Particles.init({
+        selector:'.particle',
+        maxParticles: 150,
+        connectParticles: true,
+        color: "#ffde2e",
+        speed: 1.5
+    });
+};
