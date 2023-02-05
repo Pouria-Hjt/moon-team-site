02ac298264ff787c4939aa68ff4bfe17d9cde6ec
pouria <poriw.hjt@gmail.com>
2023-02-06 00:35:35 +0330

    add page slider

--- public/js/index.js
+++ public/js/index.js
@@ -22,3 +22,26 @@ var options = {
     smartBackspace: true,
 };
 var typed = new Typed('.typed', options);
+
+/// change layout
+
+const $ = document
+const mainPage = $.querySelector(".pages")
+const pageSelector = $.querySelectorAll(".heading-secondary span")
+const pageMove = [
+    {title: "درباره", left: "-100%" , top: "100%"},
+    {title: "درباره تیم", left: "-100%" , top: "-100%"},
+    {title: "نمونه کار", left: "100%" , top: "100%"},
+    {title: "تماس با ما", left: "100%" , top: "-100%"}
+]
+
+pageSelector.forEach(selector  => {
+    selector.addEventListener("click", (event)=> {
+        let currentPage = pageMove.find(pages => {
+            return pages.title == selector.innerHTML
+        })
+        mainPage.style.transform = "translateX(" + currentPage.left + ") translateY(" + currentPage.top+ ")"
+
+        console.log(currentPage.left)
+    })
+})
