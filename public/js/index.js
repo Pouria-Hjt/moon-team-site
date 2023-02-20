1198e02e9f6373ae252e5d0213c912fea1fe2836
pouria <poriw.hjt@gmail.com>
2023-02-20 16:16:18 +0330

    add some future to teamBox

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,85 @@
-window.onload = function() {
+const $ = document
+///====================== Handle teamCard 
+
+const leaderParent = $.querySelector(".leader .card-container .swiper-wrapper")
+const coLeaderParent = $.querySelector(".co-leader .card-container .swiper-wrapper")
+const designerParent = $.querySelector(".designer .card-container .swiper-wrapper")
+const sponsorParent = $.querySelector(".sponsor .card-container .swiper-wrapper")
+
+let leader = [
+    {"image": "public/img/abol.png", "imageAlt": "parzival" , "name": "ابوالفضل محمدی" , "age": "۲۱", "ability": "فول استک دولوپر"},
+    {"image": "public/img/skadi.png", "imageAlt": "skadi" , "name": "امیرحسین محبی" , "age": "۱۸", "ability": "دولوپر دیسکورد"},
+]
+let coLeader = [
+    {"image": "public/img/pouria.jpg", "imageAlt": "pouria" , "name": "پوریا حاجتی" , "age": "۱۸", "ability": "طراح سایت"},
+    {"image": "public/img/ludho.jpg", "imageAlt": "ludho" , "name": "لودو محمد پور" , "age": "۲۰", "ability": "دولوپر دیسکورد"},
+    {"image": "public/img/smath.png", "imageAlt": "smath" , "name": "امیرعلی شهسواری" , "age": "۲۱", "ability": "دولوپر دیسکورد"},
+    {"image": "public/img/Viper_gorgi.png", "imageAlt": "viper" , "name": "سعید وایپر" , "age": "۱۹", "ability": "کانفیگر دیسکورد"},
+]
+let designer = [
+    {"image": "public/img/maryam.jpg", "imageAlt": "maryam" , "name": "مریم اوشن" , "age": "۲۰", "ability": "طراح گرافیک"},
+    {"image": "public/img/matin.png", "imageAlt": "matin" , "name": "محمد متین پایان" , "age": "۱۸", "ability": "طراح گرافیک"},
+]
+let sponsor = [
+    {"image": "public/img/sadra.png", "imageAlt": "sadra" , "name": "صدرا فرج پور" , "age": "۱۸", "ability": "استریمر"},
+    {"image": "public/img/sepehr.png", "imageAlt": "sepehr" , "name": "سپهر کتابی" , "age": "۲۱", "ability": "دولوپر دیسکورد"},
+]
+function teamCard (list , cardParent) {
+    let cardFragment = new DocumentFragment()
+    list.forEach(item => {
+        let newCard = `<div class="card swiper-slide">
+                            <img src="${item.image}" alt="${item.imageAlt}" class="card__image">
+                            <p class="card__name">${item.name}</p><p class="card__age">سن : ${item.age}</p>
+                            <span class="card__ability">${item.ability}</span>
+                         </div>`
+        cardFragment.append(newCard)
+    })
+    cardParent.insertAdjacentHTML("beforeend", cardFragment.textContent)
+}
+teamCard(leader, leaderParent)
+teamCard(coLeader, coLeaderParent)
+teamCard(designer, designerParent)
+teamCard(sponsor, sponsorParent)
+
+// particle.js
+
+window.addEventListener("load", ()=> {
     Particles.init({
         selector:'.particle',
         maxParticles: 150,
         speed: 1.5,
         connectParticles: true,
-        color: "#B2BEB5"
-
+        color: "#B2BEB5",
+        responsive: [
+            {
+                breakpoint: 1200,
+                options: {
+                    maxParticles: 100
+                }
+            },
+            {
+                breakpoint: 800,
+                options: {
+                    maxParticles: 70
+                }
+            },
+            {
+                breakpoint: 600,
+                options: {
+                    maxParticles: 50
+                }
+            },
+            {
+                breakpoint: 450,
+                options: {
+                    maxParticles: 25
+                }
+            },
+        ]
     });
-};
+
+})
+
 /// typed.js
 var typed = new Typed('.typed', {
     strings: [
@@ -24,7 +96,6 @@ var typed = new Typed('.typed', {
 
 /// change layout
 
-const $ = document
 const mainPage = $.querySelector(".pages")
 const pageSelector = $.querySelectorAll(".heading-secondary span")
 let pageBtn = $.querySelectorAll(".pages__main-btn__dropdown-item")
@@ -46,14 +117,13 @@ pageSelector.forEach(selector  => {
     })
 })
 pageBtn.forEach(selector  => {
-    selector.addEventListener("click", (event)=> {
+    selector.addEventListener("click", (event) => {
         let currentPage = pageMove.find(pages => {
             return pages.title === selector.innerHTML
         })
         mainPage.style.transform = `translateX(${currentPage.left}) translateY(${currentPage.top})`
     })
 })
-
 const BtnContainer = $.querySelector('.pages__main-btn__container')
 const dropDownContainer = $.querySelector('.pages__main-btn__dropdown')
 BtnContainer.addEventListener("mouseenter", (event) => {
@@ -72,7 +142,7 @@ dropDownContainer.addEventListener("mouseleave", (event) => {
 BtnContainer.addEventListener("click", (event) => {
     BtnContainer.parentElement.classList.toggle("change")
 })
-
+/// swiper.js
 var swiper = new Swiper(".mySwiper", {
     effect: "coverflow",
     grabCursor: true,
